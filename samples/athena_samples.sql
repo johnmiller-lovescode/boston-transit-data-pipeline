461349762dcf4c2f680b2f6aa10220bfa1f8b1fc
John Miller <johnmiller.09@yahoo.com>
2025-08-31 14:45:11 -0400

    Add sample Athena queries

--- samples/athena_samples.sql
+++ samples/athena_samples.sql
@@ -1,1 +1,15 @@
--- Add queries here once curated Parquet + Glue table exist.
+-- Vehicles per route today
+SELECT route_id, COUNT(*) AS vehicle_count
+FROM "boston-transit_db"."vehicles"
+WHERE "date" = date_format(current_date, '%Y-%m-%d')
+GROUP BY route_id
+ORDER BY vehicle_count DESC;
+
+-- Last partition present
+SHOW PARTITIONS "boston-transit_db"."vehicles";
+
+-- Peek a route
+SELECT * FROM "boston-transit_db"."vehicles"
+WHERE route_id = 'Red'
+  AND "date" = date_format(current_date, '%Y-%m-%d')
+LIMIT 50;
